0413e2d8795ae1cd44f2e574256627bb226344d4
Quinn Van Order <42633398+quinnvanorder@users.noreply.github.com>
2022-05-09 19:05:50 -0700

    Adding Operators and Ints

--- src/Hello.java
+++ src/Hello.java
@@ -2,5 +2,10 @@ public class Hello {
 
     public static void main(String[] args){
         System.out.println("Hello World");
+
+        int myFirstNumber = (10 + 5) + (2 * 10);
+        System.out.println(myFirstNumber);
+
+
     }
 }
